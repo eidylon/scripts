f725956e98dfd5512b1e4be1e30f1444749a4d4c
Aaron <5951045+eidylon@users.noreply.github.com>
2026-03-17 09:23:03 -0400

    Create findOphanVolumeShadows.ps1

---
+++ powershell/findOphanVolumeShadows.ps1
@@ -0,0 +1,22 @@
+Import-Module ActiveDirectory
+
+$searcher = New-Object DirectoryServices.DirectorySearcher
+$searcher.Filter = "(&(objectCategory=computer)(operatingSystem=*Server*)(!(name=*NTClus*))(!(name=*CLUSTER*)))"
+$searcher.PageSize = 1000
+
+$servers = $searcher.FindAll() | ForEach-Object {
+    $_.Properties["name"][0]
+} | Sort-Object -Unique
+
+Invoke-Command -ComputerName $servers -ScriptBlock {
+
+    $output = vssadmin list shadows 2>$null
+
+    if ($output -match "Persistent" -and $output -match "No auto release") {
+        [PSCustomObject]@{
+            Server = $env:COMPUTERNAME
+            Details = ($output -join "`n")
+        }
+    }
+
+} -ErrorAction SilentlyContinue
